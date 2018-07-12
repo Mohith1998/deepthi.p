82f9cf45b667b6d269ee5746c62987ff158869ca
Mohith1998 <40819079+Mohith1998@users.noreply.github.com>
2018-07-12 12:01:44 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
-a=int((input))
-if(a%2==0);
+a=int(input())
+if(a%2==0):
   print("even")
-  elif(a%1==0);
-  pritnt("odd")
- else:
- print("invalid")
+elif(a%1==0):
+  print("odd")
+else:
+  print("invalid")
